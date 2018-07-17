4fa3bea85ccc24532239766cac8416af2e415dac
bosung <bosung.kim3416@gmail.com>
2018-07-18 00:50:57 +0900

    problem 14503 solved

--- src/Main.java
+++ src/Main.java
@@ -1,78 +1,81 @@
-import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Main {
 
-    static int Answer;
-    static int[][] grid = new int[101][101];
+    static int Answer = 0;
 
-    static int[] x_d = {1, 0, -1, 0};
-    static int[] y_d = {0, -1, 0, 1};
+    public static void main(String[] args) {
+        solve();
+    }
 
-    public static void main(String args[]) throws Exception {
+    public static void solve() {
         Scanner sc = new Scanner(System.in);
 
-        // the number of dragon curves
         int N = sc.nextInt();
-        Answer = 0;
+        int M = sc.nextInt();
+
+        int r = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+
+        int[][] room = new int[N][M];
 
         for (int i = 0; i < N; i++) {
-            int x = sc.nextInt();
-            int y = sc.nextInt();
-            int d = sc.nextInt();
-            int g = sc.nextInt();
-            LinkedList<Location> curves = new LinkedList<>();
-            curves.add(new Location(x, y));
-            dragonCurve(curves, d, g, 0);
+            for (int j = 0; j < M; j++) {
+                room[i][j] = sc.nextInt();
+            }
         }
-        // check curves in square
-        for (int i = 0; i < 100; i++) {
-            for (int j = 0; j < 100; j++) {
-                if (grid[i][j] == 1 && grid[i + 1][j] == 1 && grid[i][j + 1] == 1 && grid[i + 1][j + 1] == 1) {
+
+        int curY = r;
+        int curX = c;
+        int curD = d;
+
+        int xd[] = {0, 1, 0, -1};
+        int yd[] = {-1, 0, 1, 0};
+
+        boolean nowhere = false;
+        while(true) {
+            if (room[curY][curX] == 0 || room[curY][curX] == 2) {
+                if (room[curY][curX] == 0) {
+                    room[curY][curX] = 2;
                     Answer++;
+                }
+
+                int rotate = 0;
+                // search left
+                int di = (curD - 1);
+                if (di == -1) di = 3;
+                while (true) {
+                    if (room[curY + yd[di]][curX + xd[di]] == 0) {
+                        curX = curX + xd[di];
+                        curY = curY + yd[di];
+                        curD = di;
+                        break;
+                    } else {
+                        di = di - 1;
+                        if (di == -1) di = 3;
+                        rotate++;
+                    }
+
+                    if (rotate == 4) {
+                        nowhere = true;
+                        curD = di+1;
+                        if(curD == 4) curD = 0;
+                        break;
+                    }
+                }
+            }
+            if (nowhere) {
+                if (room[curY - yd[curD]][curX - xd[curD]] == 1) {
+                    break;
+                } else {
+                    curX = curX - xd[curD];
+                    curY = curY - yd[curD];
+                    nowhere = false;
                 }
             }
         }
 
         System.out.println(Answer);
-    }
-
-    private static void dragonCurve(LinkedList<Location> curves, int d, int g, int currentGeneration) {
-        int x = curves.peekLast().x;
-        int y = curves.peekLast().y;
-
-        if (currentGeneration == 0) {
-            grid[x][y] = 1;
-            x += x_d[d];
-            y += y_d[d];
-            grid[x][y] = 1;
-            curves.add(new Location(x, y));
-        } else {
-            for (int i = curves.size() - 2; i >= 0; i--) {
-                int temp_x = curves.get(i).x - x;
-                int temp_y = curves.get(i).y - y;
-                int n_x = -temp_y + x;
-                int n_y = temp_x + y;
-                if (n_x >= 0 && n_x <= 100 && n_y >= 0 && n_y <= 100) {
-                    curves.add(new Location(n_x, n_y));
-                    grid[n_x][n_y] = 1;
-                }
-            }
-        }
-
-        if (currentGeneration == g) return;
-        else {
-            dragonCurve(curves, d, g, currentGeneration + 1);
-        }
-    }
-}
-
-class Location {
-    int x;
-    int y;
-
-    Location(int x, int y) {
-        this.x = x;
-        this.y = y;
     }
 }
---
+++ src/Problem15685.java
@@ -0,0 +1,78 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+
+class Problem15686 {
+
+    static int Answer;
+    static int[][] grid = new int[101][101];
+
+    static int[] x_d = {1, 0, -1, 0};
+    static int[] y_d = {0, -1, 0, 1};
+
+    public static void solve() throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        // the number of dragon curves
+        int N = sc.nextInt();
+        Answer = 0;
+
+        for (int i = 0; i < N; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            int d = sc.nextInt();
+            int g = sc.nextInt();
+            LinkedList<Location> curves = new LinkedList<>();
+            curves.add(new Location(x, y));
+            dragonCurve(curves, d, g, 0);
+        }
+        // check curves in square
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
+                if (grid[i][j] == 1 && grid[i + 1][j] == 1 && grid[i][j + 1] == 1 && grid[i + 1][j + 1] == 1) {
+                    Answer++;
+                }
+            }
+        }
+
+        System.out.println(Answer);
+    }
+
+    private static void dragonCurve(LinkedList<Location> curves, int d, int g, int currentGeneration) {
+        int x = curves.peekLast().x;
+        int y = curves.peekLast().y;
+
+        if (currentGeneration == 0) {
+            grid[x][y] = 1;
+            x += x_d[d];
+            y += y_d[d];
+            grid[x][y] = 1;
+            curves.add(new Location(x, y));
+        } else {
+            for (int i = curves.size() - 2; i >= 0; i--) {
+                int temp_x = curves.get(i).x - x;
+                int temp_y = curves.get(i).y - y;
+                int n_x = -temp_y + x;
+                int n_y = temp_x + y;
+                if (n_x >= 0 && n_x <= 100 && n_y >= 0 && n_y <= 100) {
+                    curves.add(new Location(n_x, n_y));
+                    grid[n_x][n_y] = 1;
+                }
+            }
+        }
+
+        if (currentGeneration == g) return;
+        else {
+            dragonCurve(curves, d, g, currentGeneration + 1);
+        }
+    }
+}
+
+class Location {
+    int x;
+    int y;
+
+    Location(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
